7436ec955afd66a627df15bd316b3671d250ca1c
jeremyHarrell <hillvulf@gmail.com>
2023-05-01 16:30:52 -0500

    complete third test in repeatSTring

--- 02_repeatString/repeatString.spec.js
+++ 02_repeatString/repeatString.spec.js
@@ -7,7 +7,7 @@ describe('repeatString', () => {
   test('repeats the string many times', () => {
     expect(repeatString('hey', 10)).toEqual('heyheyheyheyheyheyheyheyheyhey');
   });
-  test.skip('repeats the string 1 times', () => {
+  test('repeats the string 1 times', () => {
     expect(repeatString('hey', 1)).toEqual('hey');
   });
   test.skip('repeats the string 0 times', () => {
