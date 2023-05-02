2c81b35b40e0754862b3125409d2503778e43692
jeremyHarrell <hillvulf@gmail.com>
2023-05-02 13:26:35 -0500

    complete first test in sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(arg1, arg2) {
+  let sum = 0;
+  let newArray = []
+  let fullArray = []
+  for (let i = 0; i <arguments.length; i++){
+    newArray[i] = arguments[i]
+  }
+  newArray.sort((a,b) => a-b)
 
+  for (let i = newArray[0]; i <=newArray[1]; i++){
+    fullArray.push(i)
+  }
+
+  sum = fullArray.reduce((acc, curr) => acc + curr)
+  return sum
 };
 
 // Do not edit below this line
