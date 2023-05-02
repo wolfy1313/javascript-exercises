9aad2c14e30cbd415fa25ac89f1089618b0c920e
jeremyHarrell <hillvulf@gmail.com>
2023-05-02 10:26:09 -0500

    complete first test in reverseSTring

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let newString = ''
+  for (let i = string.length -1; i >= 0; i--){
+    newString+=string[i]
+    // newString.split(",").join(" ")
+  }
+  return newString
 };
 
 // Do not edit below this line
