e15e1c12d1d60c13fc8e83235f17952881500eca
jeremyHarrell <hillvulf@gmail.com>
2023-05-02 10:29:41 -0500

    clean up commented out code

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,7 +2,6 @@ const reverseString = function(string) {
   let newString = ''
   for (let i = string.length -1; i >= 0; i--){
     newString+=string[i]
-    // newString.split(",").join(" ")
   }
   return newString
 };
