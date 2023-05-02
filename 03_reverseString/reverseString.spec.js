0dc3df32e60ee45a263bb11d65638c3829c2fcaa
jeremyHarrell <hillvulf@gmail.com>
2023-05-02 10:27:18 -0500

    complete third test in reverseSTring

--- 03_reverseString/reverseString.spec.js
+++ 03_reverseString/reverseString.spec.js
@@ -9,7 +9,7 @@ describe('reverseString', () => {
     expect(reverseString('hello there')).toEqual('ereht olleh')
   })
 
-  test.skip('works with numbers and punctuation', () => {
+  test('works with numbers and punctuation', () => {
     expect(reverseString('123! abc!')).toEqual('!cba !321')
   })
   test.skip('works with blank strings', () => {
