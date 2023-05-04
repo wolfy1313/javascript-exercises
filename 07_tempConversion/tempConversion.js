566459b4a14e066b8f03d1024b1d3a2d1cc1d8cc
jeremyHarrell <hillvulf@gmail.com>
2023-05-04 12:55:52 -0500

    complete first test in tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  let answer = (degrees - 32) * 1.8
+  return Math.round(answer)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  let answer = degrees * 1.8 + 32
+  return Math.round(answer)
 };
 
 // Do not edit below this line
